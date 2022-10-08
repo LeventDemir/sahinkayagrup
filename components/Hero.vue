e7136ea9c897d6100e198e5f7baaefbe8a54ee84
LeventDemir <leventdemir518@gmail.com>
2022-10-08 13:34:39 +0300

    hero and navbar

---
+++ components/Hero.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="hero hero-bg is-primary is-large">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+      <!-- Navbar -->
+      <Navbar />
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <p class="is-size-1 has-text-black">Şahinkaya Grup</p>
+        <p class="subtitle has-text-black is-size-4">İnşaat, Hafriyat, Emlak, İletişimi Ticret</p>
+      </div>
+    </div>
+  </section>
+</template>
+
+
+<style scoped>
+.hero-bg {
+  background: url("../static/hero3.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+}
+</style>
---
+++ components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar">
+    <div class="container">
+      <div class="navbar-brand">
+        <h1 class="navbar-item is-size-2 has-text-link">Şahinkaya Grup</h1>
+        <span class="navbar-burger" data-target="navbarMenuHeroA">
+          <span></span>
+          <span></span>
+          <span></span>
+        </span>
+      </div>
+      <div id="navbarMenuHeroA" class="navbar-menu">
+        <div class="navbar-end">
+          <div class="navbar-item is-active has-text-link">Ana Sayfa</div>
+          <div class="navbar-item has-text-link">Hakkımızda</div>
+          <div class="navbar-item has-text-link">Referanslarımız</div>
+          <div class="navbar-item has-text-link">İletişim</div>
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
