686830aab68b553483eb6439da3e003a0a82af60
LeventDemir <leventdemir518@gmail.com>
2022-10-10 15:33:05 +0300

    reference page created

--- components/Card.vue
+++ components/Card.vue
@@ -2,16 +2,14 @@
     <div class="card is-clickable">
         <div class="card-image">
             <figure class="image is-4by3">
-                <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                <img src="https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="Placeholder image">
             </figure>
         </div>
         <div class="card-content">
             <div class="content">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Phasellus nec iaculis mauris. <a>@bulmaio</a>.
-                <a href="#">#css</a> <a href="#">#responsive</a>
+                Phasellus nec iaculis mauris.
                 <br>
-                <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
             </div>
         </div>
     </div>
---
+++ pages/reference.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="column is-8 is-offset-2 has-text-centered">
+        <figure class="image is-2by1 mb-6">
+            <img
+                src="https://images.pexels.com/photos/164522/pexels-photo-164522.jpeg?auto=compress&cs=tinysrgb&w=1600" />
+        </figure>
+
+        <hr>
+
+        <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
+            industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and
+            scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into
+            electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of
+            Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like
+            Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+
+        <hr />
+
+        <p class="mt-6 mb-4 pt-3">
+            <a href="tel:+905530615830">
+                <span class="has-text-black">İletişim için arayın: &nbsp; &nbsp;</span>
+                <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
+                <span class="has-text-link">+90 553 061 58 30</span>
+            </a>
+        </p>
+
+        <p>
+            <a href="//api.whatsapp.com/send?phone=+905527548500" title="Share on whatsapp" target="_blank">
+                <span class="has-text-black">İletişim için mesaj atın: &nbsp; &nbsp;</span>
+                <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
+                <span class="has-text-link">+90 553 061 5830</span>
+            </a>
+        </p>
+    </div>
+</template>
