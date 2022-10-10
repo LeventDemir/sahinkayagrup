cd2a59febd0ad3676b5fb9a27c7a84f670354e28
LeventDemir <leventdemir518@gmail.com>
2022-10-10 17:43:08 +0300

    update reference

---
+++ pages/dashboard/update-reference/_id.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="column">
+        <form>
+            <input @change="handleFileUpload" ref="photoInput" class="hide-file-input" type="file" accept="image/*"
+                :required="reference.photo ? false : true" />
+
+            <div class="field">
+                <div class="control">
+                    <button type="button" @click="$refs.photoInput.click()"
+                        class="button is-fullwidth is-success is-rounded is-outlined">Fotoğraf Yükle</button>
+                </div>
+            </div>
+
+            <div class="field">
+                <div class="control">
+                    <textarea v-model="reference.text" class="textarea" placeholder="10 lines of textarea"
+                        rows="10"></textarea>
+                </div>
+            </div>
+
+            <div class="field mb-6">
+                <div class="control">
+                    <div class="select is-rounded is-fullwidth">
+                        <select v-model="reference.status">
+                            <option value="standart">Standart</option>
+                            <option value="home-page">Anasayfa</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+
+            <div class="has-text-centered">
+                <button class="button is-link is-rounded is-outlined">Yükle</button>
+            </div>
+        </form>
+    </div>
+</template>
+    
+    
+<script>
+export default {
+    name: "update-reference",
+    data() {
+        return {
+            reference: {
+                photo: null,
+                text: null,
+                status: "standart"
+            },
+        };
+    },
+    methods: {
+        handleFileUpload(e) {
+            const files = e.target.files || e.dataTransfer.files;
+            if (!files.length) return;
+            const reader = new FileReader();
+            if (files[0].size / (1024 * 1024) < 6) {
+                const vm = this;
+                reader.onload = (e) => (vm.reference.photo = e.target.result);
+                reader.readAsDataURL(files[0]);
+                this.reference.photo = "";
+            } else
+                this.$toast.error("Yükleyeceğiniz fotoğraf 6 mb dan küçük olmalıdır!");
+        }
+    }
+};
+</script>
