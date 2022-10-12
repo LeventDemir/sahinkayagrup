bb561cd1e10cd5ae84dbb6097722c1ace668c35c
LeventDemir <leventdemir518@gmail.com>
2022-10-12 17:41:58 +0300

    references

--- pages/references.vue
+++ pages/references.vue
@@ -2,10 +2,18 @@
     <div class="mb-6 pb-6">
         <div id="references" class="column mt-6 pt-6">
             <div class="columns is-centered is-multiline">
-                <div v-for="i in 4" :key="i" class="column is-one-third">
-                    <!-- <Card /> -->
+                <div v-for="reference in $store.getters['reference/getReferences']" :key="reference._id"
+                    class="column is-one-third">
+                    <Card :data="reference" />
                 </div>
             </div>
         </div>
     </div>
 </template>
+
+
+<script>
+export default {
+
+}
+</script>
