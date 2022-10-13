f6b32fd3d0f27f6ce171b005cc92075866d22652
LeventDemir <leventdemir518@gmail.com>
2022-10-13 14:14:38 +0300

    textarea

--- pages/dashboard/update-reference/_id.vue
+++ pages/dashboard/update-reference/_id.vue
@@ -1,72 +1,87 @@
 <template>
-    <div class="column">
-        <form @submit.prevent="$store.dispatch('reference/update', reference)">
-            <input @change="handleFileUpload" ref="photoInput" class="hide-file-input" type="file" accept="image/*"
-                :required="reference.photo ? false : true" />
+  <div class="column">
+    <form @submit.prevent="$store.dispatch('reference/update', reference)">
+      <input
+        @change="handleFileUpload"
+        ref="photoInput"
+        class="hide-file-input"
+        type="file"
+        accept="image/*"
+        :required="reference.photo ? false : true"
+      />
 
-            <div class="field">
-                <div class="control">
-                    <button type="button" @click="$refs.photoInput.click()"
-                        class="button is-fullwidth is-success is-rounded is-outlined">Fotoğraf Yükle</button>
-                </div>
-            </div>
+      <div class="field">
+        <div class="control">
+          <button
+            type="button"
+            @click="$refs.photoInput.click()"
+            class="button is-fullwidth is-success is-rounded is-outlined"
+          >
+            Fotoğraf Yükle
+          </button>
+        </div>
+      </div>
 
-            <div class="field">
-                <div class="control">
-                    <textarea v-model="reference.description" class="textarea" placeholder="10 lines of textarea"
-                        rows="10"></textarea>
-                </div>
-            </div>
+      <div class="field">
+        <div class="control">
+          <textarea
+            v-model="reference.description"
+            class="textarea"
+            placeholder="10 lines of textarea"
+            rows="10"
+          ></textarea>
+        </div>
+      </div>
 
-            <div class="field mb-6">
-                <div class="control">
-                    <div class="select is-rounded is-fullwidth">
-                        <select v-model="reference.status">
-                            <option :value="false">Standart</option>
-                            <option :value="true">Anasayfa</option>
-                        </select>
-                    </div>
-                </div>
-            </div>
+      <div class="field mb-6">
+        <div class="control">
+          <div class="select is-rounded is-fullwidth">
+            <select v-model="reference.status">
+              <option :value="false">Standart</option>
+              <option :value="true">Anasayfa</option>
+            </select>
+          </div>
+        </div>
+      </div>
 
-            <div class="has-text-centered">
-                <button class="button is-link is-rounded is-outlined">Güncelle</button>
-            </div>
-        </form>
-    </div>
+      <div class="has-text-centered">
+        <button class="button is-link is-rounded is-outlined">Güncelle</button>
+      </div>
+    </form>
+  </div>
 </template>
     
     
 <script>
 export default {
-    middleware: "admin",
-    mounted() {
-        this.$store
-            .dispatch("reference/reference", this.$route.params.id)
-            .then((response) => {
-                response.id = response._id;
-                delete response._id;
-                this.reference = response;
-            });
+  middleware: "admin",
+  mounted() {
+    this.$store
+      .dispatch("reference/reference", this.$route.params.id)
+      .then((response) => {
+        response.id = response._id;
+        delete response._id;
+        this.reference = response;
+      });
+  },
+  data() {
+    return {
+      reference: {},
+    };
+  },
+  methods: {
+    handleFileUpload(e) {
+      const files = e.target.files || e.dataTransfer.files;
+      if (!files.length) return;
+      const reader = new FileReader();
+      if (files[0].size / (1024 * 1024) < 6) {
+        const vm = this;
+        reader.onload = (e) => (vm.reference.photo = e.target.result);
+        reader.readAsDataURL(files[0]);
+        this.reference.photo = "";
+      } else
+        this.$toast.error("Yükleyeceğiniz fotoğraf 6 mb dan küçük olmalıdır!");
     },
-    data() {
-        return {
-            reference: {},
-        };
-    },
-    methods: {
-        handleFileUpload(e) {
-            const files = e.target.files || e.dataTransfer.files;
-            if (!files.length) return;
-            const reader = new FileReader();
-            if (files[0].size / (1024 * 1024) < 6) {
-                const vm = this;
-                reader.onload = (e) => (vm.reference.photo = e.target.result);
-                reader.readAsDataURL(files[0]);
-                this.reference.photo = "";
-            } else
-                this.$toast.error("Yükleyeceğiniz fotoğraf 6 mb dan küçük olmalıdır!");
-        }
-    }
+  },
 };
 </script>
--- pages/reference/_id.vue
+++ pages/reference/_id.vue
@@ -1,40 +1,59 @@
 <template>
-    <div class="column is-8 is-offset-2 has-text-centered">
-        <figure class="image is-2by1 mb-6">
-            <img :src="reference.photo" />
-        </figure>
+  <div class="column is-8 is-offset-2 has-text-centered">
+    <figure class="image is-2by1 mb-6">
+      <img :src="reference.photo" />
+    </figure>
 
-        <hr>
+    <hr />
 
-        <p>{{ reference.description }}</p>
+    <p>
+      <textarea
+        class="textarea has-fixed-size is-read"
+        readonly
+        :draggable="false"
+        v-model="reference.description"
+      />
+    </p>
 
-        <hr />
+    <hr />
 
-        <p class="mt-6 mb-4 pt-3">
-            <a href="tel:+905530615830">
-                <span class="has-text-black">İletişim için arayın: &nbsp; &nbsp;</span>
-                <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
-                <span class="has-text-link">+90 553 061 58 30</span>
-            </a>
-        </p>
+    <p class="mt-6 mb-4 pt-3">
+      <a href="tel:+905530615830">
+        <span class="has-text-black">İletişim için arayın: &nbsp; &nbsp;</span>
+        <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
+        <span class="has-text-link">+90 553 061 58 30</span>
+      </a>
+    </p>
 
-        <p>
-            <a href="//api.whatsapp.com/send?phone=+905527548500" title="Share on whatsapp" target="_blank">
-                <span class="has-text-black">İletişim için mesaj atın: &nbsp; &nbsp;</span>
-                <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
-                <span class="has-text-link">+90 553 061 5830</span>
-            </a>
-        </p>
-    </div>
+    <p>
+      <a
+        href="//api.whatsapp.com/send?phone=+905527548500"
+        title="Share on whatsapp"
+        target="_blank"
+      >
+        <span class="has-text-black"
+          >İletişim için mesaj atın: &nbsp; &nbsp;</span
+        >
+        <span class="pr-2"><i class="fa-solid fa-phone"></i></span>
+        <span class="has-text-link">+90 553 061 5830</span>
+      </a>
+    </p>
+  </div>
 </template>
-
 
 <script>
 export default {
-    async asyncData({ params, store }) {
-        return store.dispatch("reference/reference", params.id).then((response) => {
-            return { reference: response };
-        });
-    },
+  async asyncData({ params, store }) {
+    return store.dispatch("reference/reference", params.id).then((response) => {
+      return { reference: response };
+    });
+  },
+
+  mounted() {
+    const textarea = document.getElementsByTagName("textarea")[0];
+    const height = textarea.scrollHeight;
+
+    textarea.style.cssText = "height:" + height + "px";
+  },
 };
 </script>
