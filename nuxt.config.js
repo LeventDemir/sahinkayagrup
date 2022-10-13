0bdd8f5a9e61c1bf64345bb31ca1f329edf71533
LeventDemir <leventdemir518@gmail.com>
2022-10-13 18:37:51 +0300

    :(

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://sahinkaya-grup.vercel.app/api',
+    baseURL: 'http://sahinkaya-grup.vercel.app/api',
   },
 
   toast: {
