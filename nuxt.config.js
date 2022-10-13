5c76aa5d0d4438a90ed9003f6c6f070b50390850
LeventDemir <leventdemir518@gmail.com>
2022-10-13 19:24:02 +0300

    turn back

--- nuxt.config.js
+++ nuxt.config.js
@@ -60,7 +60,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'http://sahinkaya-grup.vercel.app/api',
+    baseURL: process.env.BASE_URL || 'http://localhost:3000/api',
   },
 
   toast: {
@@ -90,7 +90,7 @@ export default {
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
     manifest: {
-      lang: 'en'
+      lang: 'tr'
     }
   },
 
