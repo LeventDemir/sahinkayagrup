def0a2ac524f991ff31f1497daf7a303efe8838b
LeventDemir <leventdemir518@gmail.com>
2022-10-13 16:24:34 +0300

    alt_logo

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,11 +1,15 @@
 <template>
   <footer id="contacts" class="footer">
     <div class="content has-text-centered has-text-info">
-      <p class="is-size-3 has-text-info mb-6 mt-4">Şahinkaya Grup</p>
+      <p class="mb-5">
+        <img src="/alt_logo.png" width="150" />
+      </p>
 
       <p class="mb-6">
         <span class="pr-2"><i class="fa fa-map-location-dot"></i></span>
-        <span>skiçeşme Mah. Sadi Irmak Cad. No: 19/B Gümbet - Bodrum - Muğla</span>
+        <span
+          >skiçeşme Mah. Sadi Irmak Cad. No: 19/B Gümbet - Bodrum - Muğla</span
+        >
       </p>
 
       <p class="mb-6">
